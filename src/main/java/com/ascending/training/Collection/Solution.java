3c89b2e46c0c49fe1d39fe4934d3fb9fd830f584
MengyuanWei <rebec0916@gwu.edu>
2020-06-22 16:17:14 -0400

    Try101

--- src/main/java/com/ascending/training/Collection/Solution.java
+++ src/main/java/com/ascending/training/Collection/Solution.java
@@ -37,9 +37,9 @@ public class Solution {
         //sorting object array
         Employee[] empArr = new Employee[4];
         empArr[0] = new Employee(10, "Mikey", 25, 10000);
-        empArr[1] = new Employee(20, "Arun", 29, 20000);
-        empArr[2] = new Employee(5, "Lisa", 35, 5000);
-        empArr[3] = new Employee(1, "Pankaj", 32, 50000);
+        empArr[1] = new Employee(20, "Arun", 29, 50000);
+        empArr[2] = new Employee(5, "Lisa", 35, 80000);
+        empArr[3] = new Employee(1, "Pankaj", 32, 75000);
 
         //sorting employees array using Comparable interface implementation
         Arrays.sort(empArr);
